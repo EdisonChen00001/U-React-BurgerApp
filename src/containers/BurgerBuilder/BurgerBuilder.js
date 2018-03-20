8f4fdf676f5ed65e23b014d322775e4040f63c71
zhouH0901 <zhezhou@my.bridgeport.edu>
2018-03-20 11:43:48 -0400

    add burger builder and ingredient

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import Zux from '../../hoc/Zux';
-
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component{
     render(){
         return(
             <Zux>
-                <div>Burger</div>
+                <Burger/>
                 <div>Build Controls</div>
             </Zux>
 
